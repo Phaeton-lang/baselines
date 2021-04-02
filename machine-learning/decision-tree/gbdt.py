b0def3b327ede89d2195610af311c4581f4c8cbf
Json Lee <lijiansong@ict.ac.cn>
2021-04-02 17:34:54 +0800

    update decision tree testcase

--- machine-learning/decision-tree/gbdt.py
+++ machine-learning/decision-tree/gbdt.py
@@ -1,6 +1,6 @@
+from __future__ import print_function
 import tensorflow as tf
 import numpy as np
-import copy
 
 import argparse
 from datetime import datetime
@@ -16,21 +16,6 @@ REFs:
 - <https://github.com/aymericdamien/TensorFlow-Examples>
 '''
 
-num_classes = 2 # Total classes: greater or equal to $23,000, or not (See notes below).
-num_features = 13 # data features size.
-
-# Training parameters.
-max_steps = 2000
-batch_size = 256
-learning_rate = 1.0
-l1_regul = 0.0
-l2_regul = 0.1
-
-# GBDT parameters.
-num_batches_per_layer = 1000
-num_trees = 10
-max_depth = 4
-
 parser = argparse.ArgumentParser()
 # LMS parameters
 lms_group = parser.add_mutually_exclusive_group(required=False)
@@ -45,53 +30,49 @@ if args.lms:
     tf.config.experimental.set_lms_enabled(True)
     tf.experimental.get_peak_bytes_active(0)
 
-#from tensorflow.keras.datasets import boston_housing
-#(x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+img_h, img_w = 32, 32
+num_features = img_h*img_w
+# NOTE: boosted_trees.py, for now pruning is not supported with multi class!!!
+# So num_classes can only be binary class.
+num_classes = 2
+num_points = 1000
 
-with np.load('boston_housing.npz', allow_pickle=True) as f:
-    x = f['x']
-    y = f['y']
-seed = 113
-test_split = 0.2
-np.random.seed(seed)
-indices = np.arange(len(x))
-np.random.shuffle(indices)
-x = x[indices]
-y = y[indices]
-x_train = np.array(x[:int(len(x) * (1 - test_split))])
-y_train = np.array(y[:int(len(x) * (1 - test_split))])
-#x_test = np.array(x[int(len(x) * (1 - test_split)):])
-#y_test = np.array(y[int(len(x) * (1 - test_split)):])
+x_train = np.random.randn(num_points, num_features).astype(np.int32)
+y_train = np.random.randint(num_classes, size=num_points)
 
-print('x_train shape: {}'.format(x_train.shape))
-print('y_train shape: {}'.format(y_train.shape))
+# x_train shape: (455, 13), where 13 is num_features.
+# y_train shape: (455,)
+print('===> x_train shape: {}'.format(x_train.shape))
+print('===> y_train shape: {}'.format(y_train.shape))
 
-# For classification purpose, we build 2 classes: price greater or lower than $23,000
-def to_binary_class(y):
-    for i, label in enumerate(y):
-        if label >= 23.0:
-            y[i] = 1
-        else:
-            y[i] = 0
-    return y
+# Training parameters.
+max_steps = 10
+batch_size = 512
+learning_rate = 1.0
+l1_regul = 0.0
+l2_regul = 0.1
 
-y_train_binary = to_binary_class(copy.deepcopy(y_train))
-#y_test_binary = to_binary_class(copy.deepcopy(y_test))
+NUM_EXAMPLES = len(y_train)
+def make_input_fn(X, y, shuffle=True, num_epochs=None):
+    def input_fn():
+        dataset = tf.data.Dataset.from_tensor_slices(({'x': X}, y))
+        if shuffle:
+            dataset = dataset.shuffle(NUM_EXAMPLES)
+        dataset = dataset.repeat(num_epochs).batch(batch_size)
+        return dataset
+    return input_fn
 
-train_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
-    x={'x': x_train}, y=y_train_binary,
-    batch_size=batch_size, num_epochs=None, shuffle=True)
+#train_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
+#    x={'x': x_train}, y=y_train,
+#    batch_size=batch_size, num_epochs=None, shuffle=True)train_input_fn = make_input_fn(x_train, y_train, num_epochs=10)
 
-#test_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
-#    x={'x': x_test}, y=y_test_binary,
-#    batch_size=batch_size, num_epochs=1, shuffle=False)
-
-#test_train_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
-#    x={'x': x_train}, y=y_train_binary,
-#    batch_size=batch_size, num_epochs=1, shuffle=False)
-
-# GBDT Models from TF Estimator requires 'feature_column' data format.
+train_input_fn = make_input_fn(x_train, y_train, num_epochs=1)
 feature_columns = [tf.feature_column.numeric_column(key='x', shape=(num_features,))]
+
+# GBDT parameters.
+num_batches_per_layer = 1000
+num_trees = 10
+max_depth = 4
 
 gbdt_classifier = tf.estimator.BoostedTreesClassifier(
     n_batches_per_layer=num_batches_per_layer,
@@ -104,7 +85,9 @@ gbdt_classifier = tf.estimator.BoostedTreesClassifier(
     l2_regularization=l2_regul
 )
 
+print('===========> reach here!!! before train!!!')
 gbdt_classifier.train(train_input_fn, max_steps=max_steps)
+print('===========> reach here!!! after train!!!')
 print('peak active bytes(MB): {}'.format(tf.experimental.get_peak_bytes_active(0)/1024.0/1024.0))
 print('bytes in use(MB): {}'.format(tf.experimental.get_bytes_in_use(0)/1024.0/1024.0))
 
