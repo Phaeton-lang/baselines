8aff4d5d08a2821b2c15f78c0bcf882acbd46bbc
Json Lee <lijiansong@ict.ac.cn>
2021-04-15 10:13:55 +0800

    add variable params for bert

--- tensorflow/bert-tf2/gpu_movie_reviews.py
+++ tensorflow/bert-tf2/gpu_movie_reviews.py
@@ -1,8 +1,6 @@
 import math
 import datetime
-
 from tqdm import tqdm
-
 import pandas as pd
 import numpy as np
 
@@ -17,6 +15,32 @@ from bert.tokenization.bert_tokenization import FullTokenizer
 from tensorflow import keras
 import os
 import re
+
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", type=int,
+                    default=2,
+                    help='Number of epochs to run. (Default 2)')
+parser.add_argument("--steps", type=int,
+                    default=5,
+                    help='Number of steps per epoch. (Default 5)')
+parser.add_argument("--batch_size", type=int,
+                    default=48,
+                    help='Batch size. (Default 48)')
+# LMS parameters
+lms_group = parser.add_mutually_exclusive_group(required=False)
+lms_group.add_argument('--lms', dest='lms', action='store_true',
+                       help='Enable LMS')
+lms_group.add_argument('--no-lms', dest='lms', action='store_false',
+                       help='Disable LMS (Default)')
+parser.set_defaults(lms=False)
+args = parser.parse_args()
+
+if args.lms:
+    tf.config.experimental.set_lms_enabled(True)
+    tf.experimental.get_peak_bytes_active(0)
 
 # Load all files from a directory in a DataFrame.
 def load_directory_data(directory):
@@ -54,8 +78,6 @@ def download_and_load_datasets(force_download=False):
 import bert
 from bert import BertModelLayer
 from bert.loader import StockBertConfig, map_stock_config_to_params, load_stock_weights
-#from bert.tokenization import FullTokenizer
-
 
 class MovieReviewData:
     DATA_COLUMN = "sentence"
@@ -78,7 +100,7 @@ class MovieReviewData:
         ((self.train_x, self.train_y),
          (self.test_x, self.test_y)) = map(self._prepare, [train, test])
 
-        print("max seq_len", self.max_seq_len)
+        print("max seq_len:", self.max_seq_len)
         self.max_seq_len = min(self.max_seq_len, max_seq_len)
         ((self.train_x, self.train_x_token_types),
          (self.test_x, self.test_x_token_types)) = map(self._pad,
@@ -114,7 +136,6 @@ bert_ckpt_file   = os.path.join(bert_ckpt_dir, "bert_model.ckpt")
 bert_config_file = os.path.join(bert_ckpt_dir, "bert_config.json")
 
 # perparing data
-
 tokenizer = FullTokenizer(vocab_file=os.path.join(bert_ckpt_dir, "vocab.txt"))
 data = MovieReviewData(tokenizer,
                        sample_size=10*128*2,#5000,
@@ -127,13 +148,18 @@ print("            train_y:", data.train_y.shape)
 print("             test_x:", data.test_x.shape)
 print("        max_seq_len:", data.max_seq_len)
 
+#            train_x: (2560, 128)
+#train_x_token_types: (2560, 128)
+#            train_y: (2560,)
+#             test_x: (2560, 128)
+#        max_seq_len: 128
+
 def flatten_layers(root_layer):
     if isinstance(root_layer, keras.layers.Layer):
         yield root_layer
     for layer in root_layer._layers:
         for sub_layer in flatten_layers(layer):
             yield sub_layer
-
 
 def freeze_bert_layers(l_bert):
     """
@@ -146,12 +172,10 @@ def freeze_bert_layers(l_bert):
             layer.trainable = False
         l_bert.embeddings_layer.trainable = False
 
-
 def create_learning_rate_scheduler(max_learn_rate=5e-5,
                                    end_learn_rate=1e-7,
                                    warmup_epoch_count=10,
                                    total_epoch_count=90):
-
     def lr_scheduler(epoch):
         if epoch < warmup_epoch_count:
             res = (max_learn_rate/warmup_epoch_count) * (epoch + 1)
@@ -159,14 +183,11 @@ def create_learning_rate_scheduler(max_learn_rate=5e-5,
             res = max_learn_rate*math.exp(math.log(end_learn_rate/max_learn_rate)*(epoch-warmup_epoch_count+1)/(total_epoch_count-warmup_epoch_count+1))
         return float(res)
     learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(lr_scheduler, verbose=1)
-
     return learning_rate_scheduler
 
 def create_model(max_seq_len, adapter_size=64):
   """Creates a classification model."""
-
   #adapter_size = 64  # see - arXiv:1902.00751
-
   # create the bert layer
   with tf.io.gfile.GFile(bert_config_file, "r") as reader:
       bc = StockBertConfig.from_json_string(reader.read())
@@ -179,7 +200,7 @@ def create_model(max_seq_len, adapter_size=64):
   # output         = bert([input_ids, token_type_ids])
   output         = bert(input_ids)
 
-  print("bert shape", output.shape)
+  print("bert shape:", output.shape)
   cls_out = keras.layers.Lambda(lambda seq: seq[:, 0, :])(output)
   cls_out = keras.layers.Dropout(0.5)(cls_out)
   logits = keras.layers.Dense(units=768, activation="tanh")(cls_out)
@@ -201,9 +222,7 @@ def create_model(max_seq_len, adapter_size=64):
   model.compile(optimizer=keras.optimizers.Adam(),
                 loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                 metrics=[keras.metrics.SparseCategoricalAccuracy(name="acc")])
-
   model.summary()
-
   return model
 
 adapter_size = None # use None to fine-tune all of BERT
@@ -212,11 +231,13 @@ model = create_model(data.max_seq_len, adapter_size=adapter_size)
 log_dir = ".log/movie_reviews/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%s")
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir)
 
-total_epoch_count = 50
+total_epoch_count = args.epochs
 # model.fit(x=(data.train_x, data.train_x_token_types), y=data.train_y,
+start_time = int(round(time.time()*1000))
 model.fit(x=data.train_x, y=data.train_y,
           validation_split=0.1,
-          batch_size=48,
+          batch_size=args.batch_size,
+          steps_per_epoch=args.steps,
           shuffle=True,
           epochs=total_epoch_count,
           callbacks=[create_learning_rate_scheduler(max_learn_rate=1e-5,
@@ -225,9 +246,12 @@ model.fit(x=data.train_x, y=data.train_y,
                                                     total_epoch_count=total_epoch_count),
                      keras.callbacks.EarlyStopping(patience=20, restore_best_weights=True),
                      tensorboard_callback])
-
+end_time = int(round(time.time()*1000))
+throughput = args.epochs * args.batch_size / (end_time - start_time) * 1000
+print('\n')
+print('training throughput: {}'.format(throughput))
+print('peak active bytes(MB): {}'.format(tf.experimental.get_peak_bytes_active(0)/1024.0/1024.0))
 #model.save_weights('./movie_reviews.h5', overwrite=True)
 
-_, train_acc = model.evaluate(data.train_x, data.train_y)
-
-print("train acc", train_acc)
+#_, train_acc = model.evaluate(data.train_x, data.train_y)
+#print("train acc:", train_acc)
